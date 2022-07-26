c10236c7ab315f28f89bfa8c7a9083f56973d051
Emily Bell <ebell@dropbox.com>
2022-07-26 15:49:48 -0700

    add frontend files

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-
+import { Route, Switch, Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-    DROPBOX DESTINATION!!!!!
+    <div>
+      <header>
+
+      </header>
+      DROPBOX DESTINATION!!!!!
+      <Switch>
+        <Route exact path="/" component={props => <TrailShowContainer {...props} />} />
+    </Switch>
+    </div>
     </>
   );
 };
-
-
-
 
 export default App;
