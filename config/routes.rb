a2a552b64ad6b18313cd7d7f4cb4762d348f2751
TahjDev <tahjharris00@gmail.com>
2022-07-26 02:38:18 -0700

    connected database and react to rails backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'static_pages#root'
 end
---
+++ frontend/dropbox_destinations.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom"
+import React from "react"
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("root")
+    
+    ReactDOM.render(<>React is working woooooo</>, root)
+})
