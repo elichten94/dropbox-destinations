79c7732cf4fdda94c27d3556f318aa24c0f85293
Elliot Lichtenberg <elichtenberg@dropbox.com>
2022-07-28 12:20:36 -0700

    finish migrations

--- db/migrate/20220728183821_fixc_user_id_column.rb
+++ db/migrate/20220728183821_fixc_user_id_column.rb
@@ -1,7 +1,6 @@
 class FixcUserIdColumn < ActiveRecord::Migration[6.1]
   def change
-    add_column :recommendations, :user_id, :integer, null: false 
-    remove_column :recommendations, :user_id
+    remove_column :recommendations, :user_id, :integer
     add_column :recommendations, :user_id, :integer, null: false
     add_index :recommendations, :user_id
   end
