86d2cd2a18f7ef014aabea4b039a448424e1c58a
Elliot Lichtenberg <elichtenberg@dropbox.com>
2022-07-28 13:05:10 -0700

    fix seed ordering

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+users = User.create([
+    {
+        email: 'hisnameis@elliot.com',
+        name: 'Elliot Lichtenberg',
+        city: 'Chicago',
+        state: 'Illinois',
+        country: 'USA'
+    },
+    {
+        email: 'michelle@michelle.com',
+        name: 'Michelle Chen',
+        city: 'Vancouver',
+        state: 'British Columbia',
+        country: 'Canada'
+    }
+])
+
+recs = Recommendation.create([
+    {
+        rec_name: "Revan's Cafe",
+        description: 'Not a cafe',
+        city: 'San Francisco',
+        state: 'California',
+        country: 'USA',
+        rating: 5.0,
+        user_id: 1
+    },
+    {
+        rec_name: 'Brian Na School of Photography',
+        description: 'The place to go to learn how to take pictures of your food',
+        city: 'Los Angeles',
+        state: 'California',
+        country: 'USA',
+        rating: 5.0,
+        user_id: 2
+    }
+])
+
 reviews = Review.create([
     {
         title: 'I want to come back',
@@ -22,41 +61,5 @@ reviews = Review.create([
         rec_id: 2,
         rating: 5,
         photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFPsHS2nCiB3qd3-t3nKrzj9iFYUlyGfzZ-g&usqp=CAU'
-    }
-])
-
-recs = Recommendation.create([
-    {
-        rec_name: "Revan's Cafe",
-        description: 'Not a cafe',
-        city: 'San Francisco',
-        state: 'California',
-        country: 'USA',
-        rating: 5.0
-    },
-    {
-        rec_name: 'Brian Na School of Photography',
-        description: 'The place to go to learn how to take pictures of your food',
-        city: 'Los Angeles',
-        state: 'California',
-        country: 'USA',
-        rating: 5.0
-    }
-])
-
-users = User.create([
-    {
-        email: 'hisnameis@elliot.com',
-        name: 'Elliot Lichtenberg',
-        city: 'Chicago',
-        state: 'Illinois',
-        country: 'USA'
-    },
-    {
-        email: 'michelle@michelle.com',
-        name: 'Michelle Chen',
-        city: 'Vancouver',
-        state: 'British Columbia',
-        country: 'Canada'
     }
 ])
