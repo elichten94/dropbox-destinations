dc6461aac0f5e9ae0ef45df95518f1d50b7e9f04
riousenkai <rfajardo@dropbox.com>
2022-08-01 11:02:45 -0500

    rename city_id to city_name

--- frontend/util/recommendation_api_util.js
+++ frontend/util/recommendation_api_util.js
@@ -1,6 +1,6 @@
 export const fetchCityRecommendations = (city_name) => {
   return $.ajax({
     method: "GET",
-    url: `/api/recommendations/by_city/${city_id}`    
+    url: `/api/recommendations/by_city/${city_name}`    
   })
 }
